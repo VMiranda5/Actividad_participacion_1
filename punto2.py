fadb2dda70258f0f6b0312be22a05bc7daee7b56
Valentina Miranda <vmiranda114@soyudemedellin.edu.co>
2023-08-01 13:19:09 -0500

    desarrollo punto 2

---
+++ punto2.py
@@ -0,0 +1,8 @@
+#2. Escribir una función que calcule el área de un círculo dado su radio.
+import math
+
+def area_calc(radius):
+    area = (math.pi)*(radius**2)
+    return area
+
+#print("el area del circulo es: ", area_calc(5))

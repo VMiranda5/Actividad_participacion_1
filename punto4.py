51e9c5b9a5fc8756c37282e3f28e51492bc1f692
Valentina Miranda <vmiranda114@soyudemedellin.edu.co>
2023-08-01 17:27:38 -0500

    desarrollo del punto 4

---
+++ punto4.py
@@ -0,0 +1,6 @@
+#4. Escribir un programa que determine si un número dado es par o impar
+num = int(input("ingrese un numero: "))
+if num%2 == 0:
+    print("el numero es par")
+else:
+    print("el numero es impar")

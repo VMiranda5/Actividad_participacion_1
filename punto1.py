309c643dfb42b31d072d8ab2fc4123deb29df638
Valentina Miranda <vmiranda114@soyudemedellin.edu.co>
2023-08-01 13:01:13 -0500

    desarrollo punto 1

---
+++ punto1.py
@@ -0,0 +1,5 @@
+#1. Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
+name = input("ingrese su nombre: ")
+age = int(input("ingrese su edad: "))
+print("su nombre es ", name)
+print("su edad es ", age)
